b0d7987fc00f90124f1fc694591278c17e067226
sawfiz <dashu.up@gmail.com>
2023-12-07 19:48:52 +0800

    Not show logged in user in creating new chat

--- backend/models/chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-const schema = new Schema({
-  buddies: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-});
-
-module.exports = mongoose.model('Chat', schema);
